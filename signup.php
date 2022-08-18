4bd7d0a6c21f17fd72af994925a0785f2834504a
sunil <sm31857@gmail.com>
2022-08-18 07:34:33 +0530

    final commit

---
+++ signup.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign up</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i|Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i|Roboto:300,300i,400,400i,500,700,700i&display=swap" rel="stylesheet">
+	<style> 
+		img{max-width: 100%;}
+	.main{max-width: 1202px;margin:auto;}
+      .main1{max-width: 1002px;margin: auto;}
+	*{padding: 0;margin: 0;box-sizing: border-box;}
+	.full{width: 100%;float: left;background: #f1f1f1;font-family: 'Open Sans', sans-serif;}
+	.logo{width: 100%;float: left;padding: 25px 0;text-align: center;}
+	.inner-logo{width: 100%;float: left;}
+	.inner-logo p.up{font-size: 18px;padding-top: 10px;color: #034694;font-weight: 500;}
+	.inner-logo p{font-size: 13px;color: #333;padding-top:6px; }
+	.main2{width: 500px;margin: auto;}
+	.sign{width: 100%;}
+	.inner-sign{width: 100%;float: left;text-align: center;background: white;margin-bottom: 18px;box-shadow: 0 0 12px 6px#c1c1c1;border-radius: 5px;}
+	.inner-sign input.tx{padding: 12px 305px 12px 20px;text-align: left;margin-top: 18px;}
+	.ch{fwidth: 4%;float: left;margin-left: 12px;margin-top: 30px;}
+	.su {padding: 13px 209px;margin:23px 0;color:white;background:#034694;border: none;font-size: 16px;font-weight: 500;}
+	.su:hover{background: #ef3b3b;}
+	.inner-sign p.pl{font-size: 13px;width: 95%;float: left;margin: 28px 0px 7px -10px;}
+	.inner-sign p{float: left;padding-left: 17px;font-size: 13px;color:#9d8c8c;font-family: 'Open Sans', sans-serif;margin-bottom: 20px;}
+	.inner-sign p a{text-decoration: none;}
+	.inner-sign p a:hover{color: red;}
+
+	.company{width: 100%;float: left;background: #2e2f45;padding-bottom: 70px;}
+.inner-company{width: 100%;float: left;}
+.company-1{width: 19.9%;float:left;padding-top: 125px;}
+.company-2{width: 20.1%;float:left;padding-top: 125px;}
+.company-1 p,.company-2 p{font-size: 15px;font-family: 'Open Sans', sans-serif;font-weight: 600;color: #9b9cc8;padding-bottom: 10px;}
+.company-1 ul li,.company-2 ul li{list-style: none;}
+.company-1 ul li a,.company-2 ul li a{color: #b4b5b9;font-size: 14px;font-family: 'Roboto', sans-serif;font-weight: 400;text-decoration: none;line-height: 36px;}
+.company-1 ul li a span,.company-2 ul li a span{color: white;background: red;font-size: 10px;font-family: 'Open Sans', sans-serif;font-weight: 700;padding: 1px 6px;border-radius: 4px;margin-left: 4px;}
+.quick{padding-top: 67px;}
+.banner{width: 100%;float: left;}
+.office{width: 100%;float: left;background: #d5d5e2;padding-top: 27px;padding-bottom: 30px;margin-top: -3px;}
+.inner-office{width: 100%;float: left;}
+.office-1{width: 25%;float: left;}
+.inner-office h3{font-size: 14px;font-family: 'Open Sans', sans-serif;font-weight: 600;text-transform: uppercase;}
+.pound{width: 66%;font-family: 'Open Sans', sans-serif;font-weight: 400;font-size: 13px;padding-top: 21px;}
+.com{font-family: 'Open Sans', sans-serif;font-weight: 400;font-size: 13px;padding-top: 21px;}
+.office-2{width: 37%;float: left;border-left: 2px solid #c1c1d8;padding: 0px 0px 49px 80px;border-right: 2px solid #c1c1d8;}
+.office-2 h3{font-size: 14px;font-family: 'Open Sans', sans-serif;font-weight: 600;color: #333333;padding-bottom: 10px;}
+.office-2 ul li{list-style: none;float: left;}
+.fb a::before {content: "";background: url(icon.png);height: 52px;width: 61px;display: inline-block;background-position: -26px -266px;}
+.tw a::before {content: "";background: url(icon.png);height: 52px;width: 61px;display: inline-block;background-position: -112px -266px;}
+.ins a::before {content: "";background: url(icon.png);height: 52px;width: 61px;display: inline-block;background-position: -204px -266px;}
+.yt a::before {content: "";background: url(icon.png);height: 52px;width: 61px;display: inline-block;background-position: -304px -266px;}
+.gt a::before {content: "";background: url(icon.png);height: 52px;width: 61px;display: inline-block;background-position: -392px -266px;}
+.office-3{width: 38%;float: left;padding-left: 75px;}
+.office-3 h2{font-size: 25px;font-family: 'Open Sans', sans-serif;font-weight: 700;text-transform: uppercase;color: #034694;}
+.office-3 form input{padding: 10px 22px 10px 22px;font-size: 15px;font-family: 'Open Sans', sans-serif;font-weight: 300;}
+.office-3 form  button{cursor: pointer;background: #034694;color: white;border: none;font-size: 17px;font-family: 'Roboto', sans-serif;font-weight: 500;padding: 7px 10px 11px 10px;margin-left: -4px;}
+.office-3  button::after {content: "";background: url(icon.png) no-repeat;height: 20px;width: 38px;display: inline-block;background-position: -1px -20px;}
+
+	</style>
+
+</head>
+<body>
+	<div class="full">
+	<div class="logo">
+		<div class="main2">
+			<div class="inner-logo">
+				<img src="logo.png">
+				<p class="up">Sign up in to Pound Auto Mart</p>
+				<p>Creating an account is easy. Just fill in the form below.</p>
+			</div>
+		</div>
+		
+	</div>
+<div class="sign">
+<div class="main2">
+<div class="inner-sign">	
+<form>
+	<input class="tx" type="text" name="fname" required=""  placeholder="FirstName"><br>
+	<input class="tx" type="text" name="lname" required="" placeholder="LastName"><br>
+	<input class="tx" type="text" name="email" required="" placeholder="Email"><br>
+	<input class="tx" type="password" name="psw" required="" placeholder="password"><br>
+	<input class="tx" type="password" name="psw1" required="" placeholder="confirm password"><br>
+	<input class="ch" type="checkbox" name="chk"><p class="pl">Please read & accept our term &privacy policy and be informed how we handle your personal inforamtion and keep your data secured.</p>
+
+	<input class="su" type="submit" value="Submit"><br>
+
+</form>
+
+<p>Already Registered? <a href="login.html">Sign in</a></p>
+</div>
+</div>
+</div>
+</div>
+
+<div class="company">
+	<div class="main">
+		<div class="inner-company">
+			<div class="company-1">
+				<p>COMPANY</p>
+				<ul>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Work with Us</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+					<li><a href="#">Cookie Policy</a></li>
+					<li><a href="#">Press Release</a></li>
+					<li><a href="#">Terms and Conditions</a></li>
+					<li><a href="#">Our Partners</a></li>
+					<li><a href="#">Compliants Policy</a></li>
+					<li><a href="#">Contact Us</a></li>
+					<li><a href="#">Acceptable Use</a></li>
+				</ul>
+			</div>
+
+			<div class="company-1">
+				<p>Our services</p>
+				<ul>
+					<li><a href="#">Valuations</a></li>
+					<li><a href="#">Vehicle Check</a></li>
+					<li><a href="#">Finance</a></li>
+					<li><a href="#">Insurance</a></li>
+					<li><a href="#">Gap Insurance</a></li>
+					<li><a href="#">Warranty</a></li>
+					<li><a href="#">Private Number Plate</a></li>
+					<li><a href="#">Parts & Accessories</a></li>
+					<li><a href="#">Servicing & Repair</a></li>
+					<li><a href="#">Car Rentals Worldwide  <span>HOT</span></a></li>
+					
+				</ul>
+			</div>
+
+			<div class="company-1">
+				<p>Our Vehicle Category</p>
+				<ul>
+					<li><a href="#">Cars</a></li>
+					<li><a href="#">Bikes</a></li>
+					<li><a href="#">Vans</a></li>
+					<li><a href="#">Caravans</a></li>
+					<li><a href="#">Motorhomes & Campervans</a></li>
+					<li><a href="#">Trucks</a></li>
+					<li><a href="#">Plant</a></li>
+					<li><a href="#">Tractors</a></li>
+					<li><a href="#">Other Vehicles</a></li>
+					
+				</ul>
+			</div>
+
+			<div class="company-1">
+				<p>Advertising</p>
+				<ul>
+					<li><a href="#">Display advertising</a></li>
+					<li><a href="#">Advertising Cost<span>HOT</span></a></li>
+
+				<p class="quick">Quick Links</p>
+				<ul>
+					<li><a href="#">All Used Cars in the UK</a></li>
+					<li><a href="#">All Cars by Make & Model</a></li>
+					<li><a href="#">All Used Car Dealers</a></li>
+					<li><a href="#">Find a Car dealer</a></li>
+				</ul>
+					
+				</ul>
+			</div>
+
+			<div class="company-2">
+				<p>Help & Customer Service</p>
+				<ul>
+					<li><a href="#">Rewards - How to avail & redeem<span>HOT</span></a></li>
+					<li><a href="#">Buy or Sell - Safety Guides</a></li>
+					<li><a href="#">Site Map</a></li>
+					<li><a href="#">Feedback</a></li>
+
+				<p class="quick">For Dealers in UK</p>
+				<ul>
+					<li><a href="#">Dealer Portal</a></li>
+				</ul>
+					
+				</ul>
+			</div>
+
+		</div>
+	</div>
+</div>
+<div class="banner">
+	<img src="banner-1.jpg">
+</div>
+<div class="office">
+	<div class="main">
+		<div class="inner-office">
+			<div class="office-1">
+				<h3>Registered Office Address:</h3>
+				<p class="pound">Pound Auto Mart Limited
+					Kemp House, EC1V 2NX,160 City Road, 
+					London, United Kingdom
+					<p class="com">Company Registration No. - 10818121</p>
+</p>
+			</div>
+			<div class="office-2">
+				<h3>Follow us</h3>
+				<ul>
+					<li class="fb"><a href="#"></a></li>
+					<li class="tw"><a href="#"></a></li>
+					<li class="ins"><a href="#"></a></li>
+					<li class="yt"><a href="#"></a></li>
+					<li class="gt"><a href="#"></a></li>
+				</ul>
+			</div>
+			<div class="office-3">
+				<h2>Subscribe WITH US</h2>
+				<form>
+					<input type="email" name="email" placeholder="Enter your email id">
+					<button type="button">SIGNUP</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
